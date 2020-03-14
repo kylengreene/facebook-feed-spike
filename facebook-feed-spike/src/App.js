2d7cb5ba0ab1b288751a913159fc61195c24a704
Kyle Greene <kylengreene@gmail.com>
2020-03-14 17:16:28 -0500

    ready to start quill branch

--- facebook-feed-spike/src/App.js
+++ facebook-feed-spike/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Route
+} from 'react-router-dom';
+import TextPage from './text'
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <div class="fb-page" data-href="https://www.facebook.com/KyleGreeneEV/" data-tabs="timeline" data-width=""
         data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false"
@@ -11,7 +19,14 @@ function App() {
         <blockquote cite="https://www.facebook.com/KyleGreeneEV/" class="fb-xfbml-parse-ignore"><a
           href="https://www.facebook.com/KyleGreeneEV/">Kyle Greene: Engel &amp; Völkers</a></blockquote>
       </div>
+        <Route
+          exact
+          path="/text"
+          component={TextPage}
+        />
+    
     </div>
+    </Router>
   );
 }
 
---
+++ facebook-feed-spike/src/text.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+// import { Link } from 'react-router-dom';
+// import {
+//     HashRouter as Router,
+//     Route
+// } from 'react-router-dom';
+
+
+class TextPage extends Component {
+
+
+
+
+    render() {
+    
+        return (
+            <div id="editor">
+                <p>Hello World!</p>
+                <p>Some initial <strong>bold</strong> text</p>
+                <p><br></br></p>
+</div>
+
+        )
+    }
+}
+
+export default TextPage;
